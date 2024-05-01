0c6593758a79fb3d35b5839d8e7ab831d5d04fc8
IbrohimFayzullayev <ibrohim_f01@mail.ru>
2024-05-01 23:04:56 +0500

    change localhost

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/portfolio")
+  .connect("mongodb://127.0.0.1:27017/portfolio")
   .then(() => console.log("Connected to MongoDB"))
   .catch(() => console.error("Error connecting to MongoDB...."));
 
